9b537f588e3893d849973ffd0a80b93ab1459958
Mogamat Toufeeq farat <toufeeqfarat@gmail.com>
2023-07-29 11:59:02 +0200

    testing

--- src/Components/client.jsx
+++ src/Components/client.jsx
@@ -4,5 +4,5 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_LOGIN_SUPERBASE_URL;
 const supabaseKey = import.meta.env.VITE_APP_SUPABASE_KEY;
-console.log("supabaseUrl:", supabaseUrl);
+console.log(supabaseUrl, supabaseKey)
 export const supabase = createClient(supabaseUrl, supabaseKey);
