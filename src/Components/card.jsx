706db8f558d75cd575bc376356c633161117d436
Mogamat Toufeeq farat <toufeeqfarat@gmail.com>
2023-07-29 17:18:27 +0200

    overhauled card compnonent

--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -1,38 +1,124 @@
-// Import React module
-import React from "react";
+// eslint-disable-next-line no-unused-vars
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
-// Define a functional component 'CardSetUp'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+
+// const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
 const CardSetUp = () => {
-    // Define a state variable 'card' using React's useState hook and initialize it as an empty array
-    const [card, setCard] = React.useState([]);
+
+  const [cards, setCards] = React.useState([]);
 
     // Use the useEffect hook to perform an action on component mount or when 'card' state changes
     React.useEffect(() => {
         // Fetch data from the given URL endpoint using the fetch API
         fetch("https://podcast-api.netlify.app/")
             .then(res => res.json()) // Convert the response to JSON format
-            .then(data => setCard(data)) // Update the 'card' state with the fetched data
-            // console.log(card) // Uncomment this line to log the 'card' state (not recommended in production)
-    }, [card]); // The effect will be triggered whenever the 'card' state changes
-
-    // Map over the 'card' array to create JSX elements for each card object
-    const eachCard = card.map(obj => {
-        return (
-            <div key={obj.id}>
-                {/* Display the image and title properties of each card object */}
-                <img className='card-img' src={obj.image} alt="" />
-                <span>Title: {obj.title}</span>
-            </div>
-        );
-    });
-
-    // Return the JSX to be rendered
-    return (
-        <div>
-            <span>{eachCard}</span>
-        </div>
-    );
-};
-
-// Export the 'CardSetUp' component as the default export
+            .then(data => setCards(data)) // Update the 'card' state with the fetched data
+             console.log(cards) // Uncomment this line to log the 'card' state (not recommended in production)
+    }, []);
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Album layout
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Album layout
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              Something short and leading about the collection below—its contents,
+              the creator, etc. Make it short and sweet, but not too short so folks
+              don&apos;t simply skip over it entirely.
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained">Main call to action</Button>
+              <Button variant="outlined">Secondary action</Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((cards) => (
+              <Grid item key={cards.id} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="div"
+                    sx={{
+                      // 16:9
+                      pt: '100%',
+                    }}
+                    image={cards.image}
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                    {cards.title}
+                    </Typography>
+                    <Typography>
+                       {/* {cards.description}  */}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+      </Box>
+    </ThemeProvider>
+  );
+}
 export default CardSetUp;
