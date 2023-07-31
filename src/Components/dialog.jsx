198bd659912cd0b164c547cc4baa806ed897062c
Mogamat Toufeeq farat <toufeeqfarat@gmail.com>
2023-07-31 05:22:46 +0200

    added seasons

--- src/Components/dialog.jsx
+++ src/Components/dialog.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import {
     Button,
     Dialog,
@@ -7,21 +8,31 @@ import {
     DialogActions,
     Box    
   } from '@mui/material'
-  
+  import BasicSelect from './ButtonSeasons'
   
   import { useState } from 'react'
   
    // eslint-disable-next-line react/prop-types, no-unused-vars
-   const Dialogs = ({ cardimage, cardtitle,carddescription,cardseasons,cardgenres,cardupdated ,cardid}) => {
-//     const inputTime = cardupdated;
-
-
-  
+   const Dialogs = ({ cardimage, cardtitle,carddescription,cardgenres,cardupdated ,cardid}) => {
+    const [showid, setShowid] = React.useState()
+    
+    
+    
+    React.useEffect(() => {
+      fetch(`https://podcast-api.netlify.app/id/${cardid}`)
+        .then(res => res.json())
+        .then(data => {setShowid(data);
+        })
+        .catch(error => console.error(error));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    // console.log(showid.seasons)
+    
 
     const [open, setOpen] = useState(false)
     return (
       <>
-      {console.log()}
+      
         <Button onClick={() => setOpen(true)}>Description</Button>
         <Dialog
           open={open}
@@ -33,45 +44,33 @@ import {
           <DialogContent>
           <Box
                     component="img"
-                    sx={{
-                        
+                    sx={{  
                             height: 233,
                             width: 350,
                             maxHeight: { xs: 200, md: 167 },
                             maxWidth: { xs: 200, md: 250 },
                             display: 'flex',
                             margin: 'auto'
-                        
                     }}
                     src={cardimage}
                   />
-            <DialogContentText id={cardid}>
-            
+            <DialogContentText >
                Description: {carddescription}
-
             </DialogContentText>
-            <DialogContentText id={cardid}>
-            
+            <DialogContentText >
                Updated: {cardupdated}
-
             </DialogContentText>
-            <DialogContentText id={cardid}>
-            
-            Seasons: {cardseasons}
-
-         </DialogContentText>
-         <DialogContentText id={cardid}>
-            
+            <DialogContentText >
             Genres: {cardgenres}
-
          </DialogContentText>
-
+         <DialogActions>
+         <BasicSelect  idSeasons={showid} />
+         
+         </DialogActions>
           </DialogContent>
           <DialogActions>
-            <Button onClick={() => setOpen(false)}>Cancel</Button>
-            <Button onClick={() => setOpen(false)} autoFocus>
-              Submit
-            </Button>
+            <Button onClick={() => setOpen(false)}>Close</Button>
+            
           </DialogActions>
         </Dialog>
       </>
