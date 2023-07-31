9ca1311d47b1054ff23576bdfbf14da62900ed39
Mogamat Toufeeq farat <toufeeqfarat@gmail.com>
2023-07-31 13:14:41 +0200

    changed button colors

--- src/Components/tributton.jsx
+++ src/Components/tributton.jsx
@@ -13,11 +13,11 @@ const GroupSizesColors = ({ onSortingOptionChange }) => {
         <ButtonGroup
           fullWidth
           size="large"
-          color="secondary"
+          color="primary"
           aria-label="sorting button group"
         >
-          <Button onClick={() => onSortingOptionChange('A-Z')}>A-Z</Button>
-          <Button onClick={() => onSortingOptionChange('Z-A')}>Z-A</Button>
+          <Button style={{ color:'#A305BA'  }} onClick={() => onSortingOptionChange('A-Z')}>A-Z</Button>
+          <Button style={{ color:'#A305BA'  }} onClick={() => onSortingOptionChange('Z-A')}>Z-A</Button>
         </ButtonGroup>
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
@@ -28,8 +28,8 @@ const GroupSizesColors = ({ onSortingOptionChange }) => {
           aria-label="sorting button group"
         
         >
-          <Button onClick={() => onSortingOptionChange('Ascending')}>Ascending</Button>
-          <Button onClick={() => onSortingOptionChange('Descending')}> Descending</Button>
+          <Button style={{ color:'#A305BA'  }} onClick={() => onSortingOptionChange('Ascending')}>Ascending</Button>
+          <Button style={{ color:'#A305BA'  }} onClick={() => onSortingOptionChange('Descending')}> Descending</Button>
         </ButtonGroup>
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
@@ -39,7 +39,7 @@ const GroupSizesColors = ({ onSortingOptionChange }) => {
           color="primary"
           aria-label="sorting button group"
         >
-          <Button onClick={() => onSortingOptionChange('Genre')}>Sort by Genre</Button>
+          <Button style={{ color:'#A305BA'  }} onClick={() => onSortingOptionChange('Genre')}>Sort by Genre</Button>
         </ButtonGroup>
       </Grid>
     </Grid>
