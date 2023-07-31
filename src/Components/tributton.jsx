f2c72bc3e116fb06aadf4c36b3223059fb968e6f
Mogamat Toufeeq farat <toufeeqfarat@gmail.com>
2023-07-31 12:22:13 +0200

    fixed the buttons that were overlapping

--- src/Components/tributton.jsx
+++ src/Components/tributton.jsx
@@ -1,35 +1,52 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-import Box from '@mui/material/Box';
+
+import Grid from '@mui/material/Grid';
 
 const GroupSizesColors = ({ onSortingOptionChange }) => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
+    <Grid container spacing={2} justifyContent="center" alignItems="center">
+      <Grid item xs={12} sm={6} md={4}>
+        <ButtonGroup
+          fullWidth
+          size="large"
+          color="secondary"
+          aria-label="sorting button group"
+        >
+          <Button onClick={() => onSortingOptionChange('A-Z')}>A-Z</Button>
+          <Button onClick={() => onSortingOptionChange('Z-A')}>Z-A</Button>
+        </ButtonGroup>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <ButtonGroup
+          fullWidth
+          size="large"
+          color="primary"
+          aria-label="sorting button group"
         
-        '& > *': {
-          m: 1,
-        },
-      }}
-    >
-      <ButtonGroup size="large" color="secondary" aria-label="sorting button group">
-        <Button onClick={() => onSortingOptionChange('A-Z')}>A-Z</Button>
-        <Button onClick={() => onSortingOptionChange('Z-A')}>Z-A</Button>
-      </ButtonGroup>
-      <ButtonGroup size="large" color="primary" aria-label="sorting button group">
-        <Button onClick={() => onSortingOptionChange('Ascending')}>Sort Ascending</Button>
-        <Button onClick={() => onSortingOptionChange('Descending')}>Sort Descending</Button>
-      </ButtonGroup>
-      <ButtonGroup size="large" color="primary" aria-label="sorting button group">
-        <Button onClick={() => onSortingOptionChange('Genre')}>Sort by Genre</Button>
-      </ButtonGroup>
-    </Box>
+        >
+          <Button onClick={() => onSortingOptionChange('Ascending')}> Ascending</Button>
+          <Button onClick={() => onSortingOptionChange('Descending')}> Descending</Button>
+        </ButtonGroup>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <ButtonGroup
+          fullWidth
+          size="large"
+          color="primary"
+          aria-label="sorting button group"
+        >
+          <Button onClick={() => onSortingOptionChange('Genre')}>Sort by Genre</Button>
+        </ButtonGroup>
+      </Grid>
+    </Grid>
   );
 };
 
 export default GroupSizesColors;
+
+
+
